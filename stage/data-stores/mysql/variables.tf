35b7c2066a44acf2da80cd0cb6b7cdefc079ac15
hyeinNa <nhi3373@gmail..com>
2024-02-01 04:40:12 +0000

    webserver done

---
+++ stage/data-stores/mysql/variables.tf
@@ -0,0 +1,11 @@
+variable "db_username" {
+ description = "username"
+ type = string
+ sensitive = true
+}
+
+variable "db_password" {
+ description = "password"
+ type = string
+ sensitive = true
+}
